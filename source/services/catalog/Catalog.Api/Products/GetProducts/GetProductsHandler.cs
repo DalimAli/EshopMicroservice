4e5fbe0ab1c2db101274e72f91ecaf464af40e5f
DalimAli <dalim3281@gmail.com>
2024-09-15 00:13:28 +0600

    gets products configured

---
+++ source/services/catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Catalog.Api.Products.GetProducts;
+
+public record  GetProductsQuery(): IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+public class GetProductsQueryHandler(IDocumentSession documentSession, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken) 
+    {
+        logger.LogInformation($"{nameof(GetProductsQueryHandler)} {nameof(Handle)} called with @query", query);
+
+        var products = await documentSession.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
+
