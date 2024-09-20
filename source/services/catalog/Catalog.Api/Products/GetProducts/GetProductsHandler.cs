e057297b356d496e0e9caadfabc6853eadf4bf11
DalimAli <dalim3281@gmail.com>
2024-09-21 01:02:57 +0600

    gets product paging implemented

--- source/services/catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
+++ source/services/catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -1,16 +1,19 @@
 ﻿
 
+using Marten.Pagination;
+
 namespace Catalog.Api.Products.GetProducts;
 
-public record  GetProductsQuery(): IQuery<GetProductsResult>;
+public record  GetProductsQuery(int PageNumber, int PageSize) : IQuery<GetProductsResult>;
 
-    public record GetProductsResult(IEnumerable<Product> Products);
+public record GetProductsResult(IEnumerable<Product> Products);
 
 public class GetProductsQueryHandler(IDocumentSession documentSession) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken) 
     {
-        var products = await documentSession.Query<Product>().ToListAsync(cancellationToken);
+        var products = await documentSession.Query<Product>()
+            .ToPagedListAsync(query.PageNumber, query.PageSize, cancellationToken);
 
         return new GetProductsResult(products);
     }
