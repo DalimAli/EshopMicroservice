f5f899fe8ac73554547ca587a1a135c0efc338ed
DalimAli <dalim3281@gmail.com>
2024-09-21 09:54:54 +0600

    Basket api created with initial configuration like port and global using with open api support

---
+++ source/services/basket/Basket.Api/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Mapster;
+global using MediatR;
+global using BuildingBlocks.CQRS;
+global using buildingblocks.Exceptions.Handler;
+global using buildingblocks.Behaviors;
+global using FluentValidation;
