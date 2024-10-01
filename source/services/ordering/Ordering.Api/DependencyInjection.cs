abca150f9c85d852e61468353abb4f33802a1e55
DalimAli <dalim3281@gmail.com>
2024-10-01 20:37:57 +0600

    dependency Collection registered in order api

---
+++ source/services/ordering/Ordering.Api/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Carter;
+
+namespace Ordering.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddCarter();
+
+        return services; 
+    }
+
+    public static WebApplication UseApiServices(this WebApplication webApplication)
+    {
+        webApplication.MapCarter();
+
+        return webApplication;
+    }
+}
--- source/services/ordering/Ordering.Api/Program.cs
+++ source/services/ordering/Ordering.Api/Program.cs
@@ -1,6 +1,19 @@
+using Ordering.Api;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//----------------------------------
+// Infrastructure - EF Core
+// Application - MediatR
+// API - Carter, HealthChecks...
+builder.Services.AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
