96c6517d4fbbba13f888515c11cd4c7a142256c9
DalimAli <dalim3281@gmail.com>
2024-10-03 00:47:16 +0600

    Aggregates are created and incoporated with domain

--- source/services/ordering/Ordering.Domain/Models/Order.cs
+++ source/services/ordering/Ordering.Domain/Models/Order.cs
@@ -1,12 +1,12 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class Order: Aggregate<Guid>
+public class Order: Aggregate<OrderId>
 {
     private readonly List<OrderItem> _orderItem = new();
     public IReadOnlyList<OrderItem> OrderItem => _orderItem.AsReadOnly();
 
-    public Guid CustomerId { get; set; } = default!;
-    public string OrderName { get; set; } = default!;
+    public CustomerId CustomerId { get; set; } = default!;
+    public OrderName OrderName { get; set; } = default!;
     public Address ShippingAddress { get; set; } = default!;
     public Address BillingAddress { get; set; } = default!;
     public OrderStatus Status { get; set; } = OrderStatus.Pending;
