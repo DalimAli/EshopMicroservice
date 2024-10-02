edc8b1e0cc6c6df96e42718a1c40fe954961bdd9
DalimAli <dalim3281@gmail.com>
2024-10-03 00:30:33 +0600

    Domains are created

---
+++ source/services/ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order: Aggregate<Guid>
+{
+    private readonly List<OrderItem> _orderItem = new();
+    public IReadOnlyList<OrderItem> OrderItem => _orderItem.AsReadOnly();
+
+    public Guid CustomerId { get; set; } = default!;
+    public string OrderName { get; set; } = default!;
+    public Address ShippingAddress { get; set; } = default!;
+    public Address BillingAddress { get; set; } = default!;
+    public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get => OrderItem.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
+}
