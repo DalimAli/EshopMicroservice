9a7bc6f8a98d673097154a4d72d0feb98a8337c5
DalimAli <dalim3281@gmail.com>
2024-10-06 15:55:14 +0600

    Get order by name handler created

---
+++ source/services/ordering/Ordering.Application/Orders/Queries/GetOrderByName/GetOrderByNameHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrderByName;
+
+internal class GetOrderByNameQueryHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrderByNameQuery, GetOrderByNameResult>
+{
+    public async Task<GetOrderByNameResult> Handle(GetOrderByNameQuery query, CancellationToken cancellationToken)
+    {
+        // get orders by name using db context
+        // return the result
+
+
+        var orders = await dbContext.Orders
+            .Include(x => x.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.OrderName.Value.Contains(query.Name))
+            .OrderBy(o => o.OrderName)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrderByNameResult(orders.ToOrderDtoList());
+    }
+
+
+}
