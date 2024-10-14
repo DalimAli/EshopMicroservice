14ad830eb9d0232afdf8f00786c60c5ffde1a6c2
DalimAli <dalim3281@gmail.com>
2024-10-15 00:27:23 +0600

    Catalog interface inplemented

--- source/WebApps/Eshop.ClientWeb/Services/ICatalogService.cs
+++ source/WebApps/Eshop.ClientWeb/Services/ICatalogService.cs
@@ -2,7 +2,12 @@
 
 public interface ICatalogService
 {
+    [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
     Task<GetProductsResponse> GetProductsAsync(int? pageNumber = 1, int? pageSize = 10);
+
+    [Get("/catalog-service/products/{id}")]
     Task<GetProductByIdResponse> GetProductAsync(Guid id);
+
+    [Get("/catalog-service/products/category/{category}")]
     Task<GetProductByCategoryResponse> GetProductByCategoryAsync(string category);
 }
