f7be6d71ceaa58686cb0d3899377c2b52a0748ae
DalimAli <dalim3281@gmail.com>
2024-10-17 01:40:55 +0600

    product detail implemeted

---
+++ source/WebApps/Eshop.ClientWeb/Pages/ProductDetail.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eshop.ClientWeb.Pages
+{
+    public class ProductDetailModel
+        (ICatalogService catalogService, IBasketService basketService, ILogger<ProductDetailModel> logger)
+        : PageModel
+    {
+        public ProductModel Product { get; set; } = default!;
+
+        [BindProperty]
+        public string Color { get; set; } = default!;
+
+        [BindProperty]
+        public int Quantity { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid productId)
+        {
+            var response = await catalogService.GetProductAsync(productId);
+            Product = response.Product;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+        {
+            logger.LogInformation("Add to cart button clicked");
+            var productResponse = await catalogService.GetProductAsync(productId);
+
+            var basket = await basketService.LoadUserBasket();
+
+            basket.Items.Add(new ShoppingCartItemModel
+            {
+                ProductId = productId,
+                ProductName = productResponse.Product.Name,
+                Price = productResponse.Product.Price,
+                Quantity = Quantity,
+                Color = Color
+            });
+
+            await basketService.StoreBasketAsync(new StoreBasketRequest(basket));
+
+            return RedirectToPage("Cart");
+        }
+    }
+}
